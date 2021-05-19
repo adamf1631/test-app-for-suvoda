813aed6e9ff15b2e8e4ef295a0dc78bb6a57d035
Falchetta <afalchetta@vhs1.org>
2021-05-19 10:10:17 -0400

    Main Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [color, setColor] = useState('');
+
+useEffect(() => {
+  console.log('render', color)
+}, [color])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>
+      {color}
+      </h4>
+
+      <button onClick={() => setColor('Blue')}>Blue</button>
+      <button onClick={() => setColor('Red')}>Red</button>
+      <button onClick={() => setColor('Green')}>Green</button>
     </div>
   );
 }
